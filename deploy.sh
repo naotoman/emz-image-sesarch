eef3335862e0bc73ef49d13ce3d2907e6e80aa12
naotoman <37291085+naotoman@users.noreply.github.com>
2025-08-17 18:50:24 +0900

    deployed

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,6 @@ REPO_NAME=emz/searchwithapi
 
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
-# docker build -t tmp:latest .
-
-# docker tag tmp:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:$tag
-docker build ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:$tag
+docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:$tag .
 
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:$tag
